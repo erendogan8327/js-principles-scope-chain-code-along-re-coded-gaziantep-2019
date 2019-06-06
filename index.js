6f5e33d1e88a7ad57cb4d96dc37593d197d120c2
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-06 13:28:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-/* Your code goes here */
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'osman';
+
+let favoriteCustomer = 'mahmut';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(newBest){
+  
+  bestCustomer = newBest;
+}
+
+function changeLeastFavoriteCustomer(){
+  
+  leastFavoriteCustomer = 'eren';
+  
+}
+
+
+function attemptTwoFavoriteCustomers() {
+
+  let favoriteCustomer = "hakan"
+  
+
+  
+}
+
+
+
